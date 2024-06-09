a13f97ad789bf4a451b31eb4f61578bf1c15d18f
sstvskids <126665568+sstvskids@users.noreply.github.com>
2024-06-09 11:14:57 +1000

    Update 6872274481.lua

--- CustomModules/6872274481.lua
+++ CustomModules/6872274481.lua
@@ -398,7 +398,7 @@ local function getSpeed()
 			speed = speed + 90
 		end
 		if store.scythe > tick() then
-		    if Disabler.SpeedBypassMethod.Value == "Heatseeker" then
+		    if SpeedBypassMethod.Value == "Heatseeker" then
 		        if entityLibrary.isAlive and entityLibrary.character.Head.Transparency == 0 then
 		            speed = speed + 37
 		        end
