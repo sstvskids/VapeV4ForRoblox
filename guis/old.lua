f8dfe24863e9e24933a5ba5f0f41eaac00bd38fa
sstvskids <staavstudios@gmail.com>
2025-05-17 23:41:01 +1000

    fix x2

--- guis/old.lua
+++ guis/old.lua
@@ -230,6 +230,7 @@ local function downloadFile(path, func)
 			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
 		end
 		task.spawn(function()
+			task.wait(0.06)
 			writefile(path, res)
 		end)
 	end
