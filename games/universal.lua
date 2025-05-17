75d2dd1c61b7a9e73616614dfea00b39780be64f
sstvskids <staavstudios@gmail.com>
2025-05-17 23:44:11 +1000

    Update universal.lua

--- games/universal.lua
+++ games/universal.lua
@@ -22,10 +22,7 @@ local function downloadFile(path, func)
 		if path:find('.lua') then
 			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
 		end
-		task.spawn(function()
-			task.wait(0.06)
-			writefile(path, res)
-		end)
+		writefile(path, res)
 	end
 	return (func or readfile)(path)
 end
