3db59adfcce25e6d5ceb0ce41e1ee4ba40c47d10
sstvskids <staavstudios@gmail.com>
2025-05-23 19:22:02 +1000

    Update 11630038968.lua

--- games/trashexecs/11630038968.lua
+++ games/trashexecs/11630038968.lua
@@ -256,7 +256,14 @@ run(function()
 				
 											if not Swing.Enabled and SwingDelay < tick() then
 												SwingDelay = tick() + 0.25
+
+												if vape.ThreadFix then
+													setthreadidentity(2)
+												end
 												lplr.Character.Humanoid.Animator:LoadAnimation(getTool().Animations.Swing):Play()
+												if vape.ThreadFix then
+													setthreadidentity(8)
+												end
 											end
 				
 											if delta.Magnitude > AttackRange.Value then continue end
