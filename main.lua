08990d144270fe08758a3903918729a948f79ccc
sstvskids <staavstudios@gmail.com>
2025-06-28 09:19:05 +1000

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,8 +4,13 @@ if shared.badexecs then return end
 
 -- why do exploits fail to implement anything correctly? Is it really that hard?
 if identifyexecutor then
-	if table.find({'Argon', 'Wave', 'Velocity'}, ({identifyexecutor()})[1]) then
+	if table.find({'Argon', 'Wave', 'Velocity', 'Volcano'}, ({identifyexecutor()})[1]) then
 		getgenv().setthreadidentity = function(val)
+			return val
+		end
+	end
+	if table.find({'Volcano'}, ({identifyexecutor()})[1]) then
+		getgenv().getthreadidentity = function(val)
 			return val
 		end
 	end
